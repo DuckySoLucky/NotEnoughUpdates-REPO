ca4529a9e7b23a8b0ec2a0bb5b3fb1dc0b71b0b0
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-02-23 14:12:58 +0100

    feat: Enhance recipe output structure and add NPC shop recipe handling

--- scripts/update-recipes.ts
+++ scripts/update-recipes.ts
@@ -13,49 +13,59 @@ type Recipe = {
   count: number;
 };
 
+type Position = keyof Omit<Recipe, "count">;
+
+type OutputItem = {
+  name: string;
+  recipe: Recipe;
+  wiki?: string[];
+};
+
 const POSITIONS = ["A1", "B1", "C1", "A2", "B2", "C2", "A3", "B3", "C3"];
 
-const output: { [key: string]: Recipe[] } = {};
+const formatRecipes = (recipe: string[]) =>
+  POSITIONS.reduce((acc, pos, index) => {
+    acc[pos as Position] = recipe[index] || "";
+    return acc;
+  }, {} as Recipe);
+
+const output: Record<string, OutputItem> = {};
 const items = await readdir("../items");
 
 for (const item of items) {
   const itemData = await Bun.file(`../items/${item}`).json();
-
-  const recipes = [] as Recipe[];
   if (itemData.recipe) {
-    recipes.push({
-      ...itemData.recipe,
-      count: itemData.recipe.count ?? 1,
-    });
+    output[itemData.internalname] = {
+      name: itemData.displayname.replace(/§./g, ""),
+      recipe: {
+        ...itemData.recipe,
+        count: parseInt(itemData.recipe.count ?? 1) ?? 1,
+      },
+      wiki: itemData.info,
+    };
   }
 
-  const craftingRecipes = itemData.recipes?.filter((recipe: { type: string; }) => recipe?.type === "crafting") ?? [];
+  const foundRecipes = itemData.recipes?.filter(
+    (recipe: { type: string; }) => recipe?.type === "npc_shop"
+  );
+  if (foundRecipes && foundRecipes.length) {
+    for (const recipe of foundRecipes) {
+      const newRecipe = formatRecipes(recipe.cost);
+      const [fileId, amount] = recipe.result.split(":");
+      const id = recipe.result.split(";")[0];
 
-  if (craftingRecipes.length) {
-    for (const recipe of craftingRecipes) {
-      const newRecipe = Object.fromEntries(
-        POSITIONS.map((pos, i) => [pos, recipe[pos] ?? ""])
-      );
-
-      recipes.push({
-        ...newRecipe,
-        count: recipe.count,
-      } as Recipe);
+      const itemData = await Bun.file(`../items/${fileId}.json`).json();
+      output[id] = {
+        name: itemData?.displayname.replace(/§./g, ""),
+        recipe: { ...newRecipe, count: parseInt(amount ?? 1) ?? 1 },
+        wiki: itemData?.info,
+      };
     }
   }
-
-  if (!recipes.length) {
-    continue;
-  }
-
-  output[itemData.internalname] = recipes;
 }
 
-const sorted = Object.keys(output)
-  .sort()
-  .reduce<{ [key: string]: Recipe[] }>((acc, key) => {
-    acc[key] = output[key];
-    return acc;
-  }, {});
+const sorted = Object.fromEntries(
+  Object.entries(output).sort(([a], [b]) => a.localeCompare(b))
+);
 
 await Bun.write("../recipes/recipes.json", JSON.stringify(sorted, null, 2));
