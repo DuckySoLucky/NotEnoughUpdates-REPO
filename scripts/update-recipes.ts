94bbf67ebad0d3ccadcf78178cc7418395129506
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-02-23 15:30:19 +0100

    feat: Improve recipe formatting and handling in update-recipes script

--- scripts/update-recipes.ts
+++ scripts/update-recipes.ts
@@ -1,67 +1,73 @@
 import { readdir } from "node:fs/promises";
 
-type Recipe = {
-  A1: string;
-  B1: string;
-  C1: string;
-  A2: string;
-  B2: string;
-  C2: string;
-  A3: string;
-  B3: string;
-  C3: string;
-  count: number;
-};
-
-type Position = keyof Omit<Recipe, "count">;
-
-type OutputItem = {
-  name: string;
-  recipe: Recipe;
-  wiki?: string[];
-};
-
 const POSITIONS = ["A1", "B1", "C1", "A2", "B2", "C2", "A3", "B3", "C3"];
 
-const formatRecipes = (recipe: string[]) =>
-  POSITIONS.reduce((acc, pos, index) => {
-    acc[pos as Position] = recipe[index] || "";
-    return acc;
-  }, {} as Recipe);
+async function formatRecipe(recipe: string[]) {
+  const output: Record<string, string> = {};
+  for (let i = 0; i < POSITIONS.length; i++) {
+    if (!recipe[i]) {
+      output[POSITIONS[i]] = "";
+      continue;
+    }
 
-const output: Record<string, OutputItem> = {};
+    const [NEUId, amount] = recipe[i].split(":");
+    const id = (await getItemId(NEUId)) ?? NEUId;
+
+    output[POSITIONS[i]] = `${id};${amount}`;
+  }
+
+  return output;
+}
+
+const output: { 
+  [key: string]: {
+    name: string;
+    recipe: any;
+    wiki?: string[]
+  }
+} = {};
 const items = await readdir("../items");
 
 for (const item of items) {
   const itemData = await Bun.file(`../items/${item}`).json();
   if (itemData.recipe) {
-    output[itemData.internalname] = {
-      name: itemData.displayname.replace(/§./g, ""),
-      recipe: {
-        ...itemData.recipe,
-        count: parseInt(itemData.recipe.count ?? 1) ?? 1
-      },
-      wiki: itemData.info
+    output[itemData.internalname as string] = {
+      name: itemData.displayname.replace(/ยง./g, ""),
+      recipe: await formatRecipe(Object.values(itemData.recipe)),
+      wiki: itemData.info,
     };
   }
 
-  const foundRecipes = itemData.recipes?.filter((recipe: { type: string }) => recipe?.type === "npc_shop");
+  const foundRecipes = itemData.recipes?.filter(
+    (recipe: { type: string; }) => recipe?.type === "npc_shop"
+  );
   if (foundRecipes && foundRecipes.length) {
     for (const recipe of foundRecipes) {
-      const newRecipe = formatRecipes(recipe.cost);
+      const newRecipe = await formatRecipe(recipe.cost);
       const [fileId, amount] = recipe.result.split(":");
       const id = recipe.result.split(";")[0];
 
       const itemData = await Bun.file(`../items/${fileId}.json`).json();
       output[id] = {
-        name: itemData?.displayname.replace(/§./g, ""),
+        name: itemData?.displayname.replace(/ยง./g, ""),
         recipe: { ...newRecipe, count: parseInt(amount ?? 1) ?? 1 },
-        wiki: itemData?.info
+        wiki: itemData?.info,
       };
     }
   }
 }
 
-const sorted = Object.fromEntries(Object.entries(output).sort(([a], [b]) => a.localeCompare(b)));
+async function getItemId(NEUId: string) {
+  if (NEUId === "SKYBLOCK_COIN") {
+    return NEUId;
+  }
+
+  const itemData = await Bun.file(`../items/${NEUId}.json`).json();
+  return itemData.internalname.replace("-", ":");
+}
+
+const sorted = Object.fromEntries(
+  Object.entries(output).sort(([a], [b]) => a.localeCompare(b))
+);
 
 await Bun.write("../recipes/recipes.json", JSON.stringify(sorted, null, 2));
